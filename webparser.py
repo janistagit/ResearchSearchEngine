0db68550a4a6d2616837c6b1be120753594e966e
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-11-28 10:19:02 -0800

    Add index code skeleton

--- webparser.py
+++ webparser.py
@@ -3,3 +3,23 @@ from pymongo import MongoClient
 from crawler import connectDataBase
 
 db = connectDataBase()
+index = db.index
+
+# Text transformation/parsing code
+
+# Index management code
+def createDocument(col):
+    termCount = {}
+
+    document = {}
+
+    col.insert_one(document)
+
+def deleteDocument(col):
+    pass
+
+def updateDocument(col):
+    pass
+
+def getIndex(col):
+    pass
