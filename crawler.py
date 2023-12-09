ce08bf95c0d9575007512b85a4c92ee53fdc3b79
joe559 <57023960+joe559@users.noreply.github.com>
2023-12-09 12:37:02 -0800

    target method for finding pattern

--- crawler.py
+++ crawler.py
@@ -9,3 +9,24 @@ def connectDataBase():
     client = MongoClient(host="localhost", port=27017)
     db = client.searchengine
     return db
+# Determine if URL follows faculty page pattern
+"""
+Determines if target page is found
+params: html (string)
+returns: Found(boolean)
+
+"""
+def is_target_page(html):
+    bs = BeautifulSoup(html, 'html.parser')
+    
+    # Check if the HTML contains the 'fac-info' division which indicates a faculty member's page
+    faculty_info = soup.find('div', class_='fac-info')
+    
+    # Check if within 'fac-info', there's an 'h1' tag for the faculty name, 
+    # and 'title-dept' class within a 'span' for the faculty title and department
+    if faculty_info:
+        faculty_name = faculty_info.find('h1')
+        faculty_title_dept = faculty_info.find('span', class_='title-dept')
+        # If both elements exist, it's likely a target page
+        return bool(faculty_name and faculty_title_dept)
+    return False
