18854127005646f01809b94a4073f4b3fb39ec55
joe559 <57023960+joe559@users.noreply.github.com>
2023-12-09 15:58:20 -0800

    fixed target page method

--- crawler.py
+++ crawler.py
@@ -16,20 +16,16 @@ Determines if target page is found based off faculty page pattern
 params: html (string)
 returns: Found(boolean)
 """
+
 def is_target_page(html):
+    # Create a BeautifulSoup object
     bs = BeautifulSoup(html, 'html.parser')
     
-    # Check if the HTML contains the 'fac-info' division which indicates a faculty member's page
-    faculty_info = bs.find('div', class_='fac-info')
-    
-    # Check if within 'fac-info', there's an 'h1' tag for the faculty name, 
-    # and 'title-dept' class within a 'span' for the faculty title and department
-    if faculty_info:
-        faculty_name = faculty_info.find('h1')
-        faculty_title_dept = faculty_info.find('span', class_='title-dept')
-        # If both elements exist, it's likely a target page
-        return bool(faculty_name and faculty_title_dept)
-    return False
+    # Find the 'title-dept' span
+    title_comparison = bool(bs.find('div').find(string=re.compile('.*College of Business Administration.*')))
+    if title_comparison:
+        print("Found \nTarget\n Page")
+    return title_comparison
     
 # Function to retrieve and store page in MongoDB
 def store_page(url, html):
