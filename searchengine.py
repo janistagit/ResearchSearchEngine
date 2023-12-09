84a30945b32ee0dbf87e8f450ed29f507bf6b3c7
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-12-09 01:25:37 -0800

    Add skeleton for driver program

--- searchengine.py
+++ searchengine.py
@@ -1,3 +1,23 @@
 from crawler import *
 from webparser import *
 from query import *
+
+# Driver program for search engine
+if __name__ == '__main__':
+    db = connectDataBase()
+
+    print("")
+    print("########## Search Engine ##############")
+    print("#Type a query")
+    print("#Press q to quit")
+
+    query = ""
+    while query != ("q" | "Q"):
+        query = input("Enter your query: ")
+
+        if query == ("q" | "Q"):
+            print("Exiting search engine.")
+
+        else:
+            # Insert search engine functions
+            pass
