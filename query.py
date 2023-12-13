fe8ab8040c253a18164f794ec6c2ae9619d08b01
ridasiddiqui123 <ridasiddiqui123@gmail.com>
2023-12-12 22:14:08 -0800

    Added pagination into query.py
    Added pagination

--- query.py
+++ query.py
@@ -3,11 +3,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from pymongo import MongoClient
 
+page_size = 5
+
 # Connect to MongoDB
 client = MongoClient()
 db = client.business_department
 pages = db.Professor_information
-
 
 def search_faculty(query, faculty_data):
     faculty_info = []
@@ -32,12 +33,44 @@ def search_faculty(query, faculty_data):
 
     return results
 
+
+def pagination(results, page_size):
+    total_item_count = len(results)
+    total_pages = (total_item_count + page_size - 1) // page_size
+
+    while True:
+        try:
+            page_number = int(input("Enter page number: "))
+            if 1 <= page_number <= total_pages:
+                start_index = (page_number - 1) * page_size
+                end_index = page_number * page_size
+                page_data = results[start_index:end_index]
+                for result in page_data:
+                    print(f"Cosine similarity: {result['cos_sim']}")
+                    print(f"Professor: {result['Name']}")
+                    print(f"Title: {result['Title']}")
+                    print(f"Email: {result['Email:']}")
+                    print(f"Phone: {result['Phone']}")
+                    print(f"Office: {result['Office']}")
+                    print("\n")
+            else:
+                print("Not a valid page number. Please try again.")
+                print()
+        except ValueError:
+            print("Invalid Input. Please enter a valid number.")
+            print()
+
+
 # search query and info
 query = "sales force"
 db_data = list(pages.find())
 results = search_faculty(query, db_data)
 
-# just printing main professor details for now but feel free to change it
+pagination(results, page_size)
+
+
+#commented out just in case
+'''
 for result in results:
     print(f"Cosine similarity: {result['cos_sim']}")
     print(f"Professor: {result['Name']}")
@@ -46,3 +79,4 @@ for result in results:
     print(f"Phone: {result['Phone']}")
     print(f"Office: {result['Office']}")
     print("\n")
+'''
