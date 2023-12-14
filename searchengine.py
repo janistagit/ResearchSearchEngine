d856307cc6241bf8e077a6365f32b2c790cc5894
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-12-13 22:11:40 -0800

    Call functions to perform search

--- searchengine.py
+++ searchengine.py
@@ -4,12 +4,35 @@ from query import *
 
 # Driver program for search engine
 if __name__ == '__main__':
-    db = connectDataBase()
+
+    # Initialize Database
+    db, client = connectDataBase()
+    pages_collection = db.pages
+
+    # Seed URLs for each department
+    seed_urls = ['https://www.cpp.edu/cba/international-business-marketing/index.shtml']
+    
+    # Number of target faculty pages to find (this will be department-specific)
+    num_targets = 22
+
+    # Initialize frontier with seed URLs
+    frontier = Frontier(seed_urls)
+
+    # Start crawling
+    crawlerThread(frontier, num_targets)
+    print("\n\nCrawling complete!")
+
+    # Close the MongoDB client when done
+    #client.close()
+
+    # Parse text and insert terms into index
+    textTransformation()
+    print("Index creation complete!")
 
     print("")
     print("########## Search Engine ##############")
-    print("#Type a query")
-    print("#Press q to quit")
+    print("# Type a query")
+    print("# Press q to quit")
 
     query = ""
     while query != ("q" | "Q"):
@@ -19,5 +42,5 @@ if __name__ == '__main__':
             print("Exiting search engine.")
 
         else:
-            # Insert search engine functions
-            pass
+            # Rank and query pages
+            search_engine(query, db)
